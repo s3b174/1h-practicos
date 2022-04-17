1e23ebbb13f3aa740c02f9ae12413732c3cd2882
s3b174 <s3b174@s3b174>
2022-04-16 20:26:51 -0400

    Trabajo incompleto.

---
+++ Clase2/TP1/TP1/src/TP1.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : TP1.c
+ Author      : SEBASTIAN SANTILLAN
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "msjlib.h"
+
+int main(void) {
+
+	/* Pido disculpas por entregar el trabajo sin hacer.
+	 * Estas semanas fueron muy complicadas y no puede avanzar
+	 * casi nada en las materias. Entrego el TP1 para
+	 * poder entregar el TP2. Voy a intentar organizarme
+	 * mejor estas proximas semanas. Pido disculpas nuevamente.
+	 *
+	 */
+
+}
---
+++ Clase2/TP1/TP1/src/msjlib.h
@@ -0,0 +1,13 @@
+/*
+ * msjlib.h
+ *
+ *  Created on: Apr 16, 2022
+ *      Author: s3b174
+ */
+
+#ifndef MSJLIB_H_
+#define MSJLIB_H_
+
+
+
+#endif /* MSJLIB_H_ */
